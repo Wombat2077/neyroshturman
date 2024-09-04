1ee64a018c7cabc7f9297c9419691b0f0eef6d50
wombat1742 <tomgogard@gmail.com>
2024-09-04 17:19:40 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def get_answer(text: str, chat_id=None):
             new, answer = await chat.new_chat(
             char, me.id
             )
-            return answer.text    
+          
         message = await chat.send_message(
             char, chat_id, text
         )
